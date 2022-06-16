c6da0f2287b4f53918326aa7dc4a559501d41be7
Daniel Mita <noreply@dango.space>
2022-06-16 15:59:28 +0100

    Add common dumper modules

--- tests/example-success/Leap.pm
+++ tests/example-success/Leap.pm
@@ -2,15 +2,18 @@
 package Leap;
 use strict;
 use warnings;
+use feature qw<say>;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<is_leap_year>;
 
 # For checking module presence in test runner
 use Moo;
+use Data::Dump;
 
 sub is_leap_year {
   my ($year) = @_;
-  print "OUTPUT\n" if $year == 1970;
+  say 'OUTPUT' if $year == 1970;
+  dd(['DUMPED']) if $year == 2100;
   return $year % 4 == 0 && $year % 100 != 0 || $year % 400 == 0;
 }
 
